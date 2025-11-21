12b6c38119d78d078372968ddaa9d3b9840d4fd8
mayureshsamkhya <mayuresh.kumbhar@samkhya.ai>
2025-11-21 21:46:22 +0530

    there layout provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,8 +39,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <Navbar/>
@@ -50,3 +50,11 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+//  <ThemeProvider
+//           attribute="class"
+//           defaultTheme="system"
+//           enableSystem
+//           disableTransitionOnChange
+//         ></ThemeProvider>
